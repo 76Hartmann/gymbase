3a4ed2397571183ae1b67dc8cb8b3b826d6a0ad6
jetm76 <jetm76@gmail.com>
2025-07-10 21:55:05 +0000

    GymBase 1

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html', datos=123)
+
+def main():
+    app.run(port=int(os.environ.get('PORT', 80)))
+
+if __name__ == "__main__":
+    main()
